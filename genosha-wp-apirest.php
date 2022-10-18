b752a95eb2a20c5fa2acfd80bdb9135e0b3888c7
Juan Iriart <jeiriart@gmail.com>
2022-10-18 17:47:23 -0300

    first commit

---
+++ genosha-wp-apirest.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Genosha API REST
+ * Version: 1.0.1
+ * Author: Juan Iriart
+ * Description: Genosha plugin for API REST v3
+ */
+
+ require plugin_dir_path( __FILE__ ).'/vendor/autoload.php';
