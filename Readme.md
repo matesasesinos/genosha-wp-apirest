6cbe45338053fce5ca1466bc20000c1fbf4dab31
Juan Iriart <jeiriart@gmail.com>
2022-10-19 07:04:36 -0300

    feat: authentication

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Info
+
+### Application Passwords
+
+[Doc oficial](https://make.wordpress.org/core/2020/11/05/application-passwords-integration-guide/)
--- genosha-wp-apirest.php
+++ genosha-wp-apirest.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Genosha API REST
  * Version: 1.0.1
@@ -6,4 +7,21 @@
  * Description: Genosha plugin for API REST v3
  */
 
- require plugin_dir_path( __FILE__ ).'/vendor/autoload.php';
+require plugin_dir_path(__FILE__) . '/vendor/autoload.php';
+
+
+class GenoshaApiRestInit
+{
+    private static $init = false;
+    public static function init()
+    {
+        if(self::$init) 
+            return;
+        
+        self::$init = true;
+        //Initialize APP Passwords
+        add_filter( 'wp_is_application_passwords_available', '__return_true' );
+    }
+}
+
+GenoshaApiRestInit::init();
